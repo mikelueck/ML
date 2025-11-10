72356069c64fb3ded847d988a25a78660e4d0d77
Mike Lueck <michael.lueck@payshepherd.com>
2025-11-10 09:19:27 -0500

    updated cache headers for index.html

--- canbiocin/server/push.sh
+++ canbiocin/server/push.sh
@@ -5,8 +5,9 @@ bazel build  //canbiocin/client:bundle -c opt
 
 gcloud storage rm gs://canbiocin-static-site-dev-canbiocin-474014/*
 
-
 gcloud storage cp ~/src/ML/bazel-bin/canbiocin/client/bundle/* gs://canbiocin-static-site-dev-canbiocin-474014
 gcloud storage cp ~/src/ML/bazel-bin/canbiocin/client/public/*.avif gs://canbiocin-static-site-dev-canbiocin-474014
+
+gcloud storage cp ~/src/ML/bazel-bin/canbiocin/client/bundle/index.html gs://canbiocin-static-site-dev-canbiocin-474014 --cache-control "no-store"
 
 
